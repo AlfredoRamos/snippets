813177e980fb2be31fc7337918a6d6d44769a9af
Alfredo Ramos <AlfredoRamos@users.noreply.github.com>
2016-06-08 19:45:04 -0500

    [bash] Update my mysql dump script

--- bash/mysql-dump/mysql_dump.sh
+++ bash/mysql-dump/mysql_dump.sh
@@ -1,38 +1,101 @@
-#!/bin/sh --
+#!/bin/bash --
+
+#
+# A simple script to backup my databases using cronie
+# https://github.com/AlfredoRamos/snippets/tree/master/bash/mysql-dump
+#
 
 # MySQL config
-MYSQL_HOST=<HOST>
+MYSQL_HOST=''
 MYSQL_PORT=3306
-MYSQL_DATABASE=<DATABASE>
-MYSQL_USER=<USER>
-MYSQL_PASSWORD=<PASSWORD>
-MYSQL_LOG=mysql_dump.log
+MYSQL_DATABASES=()
+MYSQL_USER=''
+MYSQL_PASSWORD=''
 
 # Backup config
-BASE_DIR=$(pwd)
-BACKUP_DIR=${BASE_DIR}/backup
-BACKUP_TIMESTAMP="$(date +'%F %T %z')"
-BACKUP_FILENAME="$(printf "%s" "${MYSQL_DATABASE} ${BACKUP_TIMESTAMP}" | sed -e 's/[-:]//g' -e 's/ /_/g')"
+BASE_DIR="$(pwd)"
+BACKUP_DIR="${BASE_DIR}"/backup
+
+# Generate filename
+# ${1}	Database/file name
+# ${2}	Timestamp format
+# ${3}	Prefix string
+# ${4}	Suffix string
+get_filename() {
+	local filename="${1}"
+	local timestamp="${2}"
+	local prefix="${3}"
+	local suffix="${4}"
+	local name=''
+
+	if [[ -z "${timestamp}" ]]; then
+		timestamp='%F %T %z'
+	fi
+
+	if [[ ! -z "${prefix}" && ! -z "${suffix}" ]]; then
+		name="${prefix} ${filename} ${suffix}"
+	elif [[ ! -z "${prefix}" && -z "${suffix}" ]]; then
+		name="${prefix} ${filename}"
+	elif [[ -z "${prefix}" && ! -z "${suffix}" ]]; then
+		name="${filename} ${suffix}"
+	else
+		name="${filename}"
+	fi
+
+	printf "$(date +"${timestamp}") ${name}" | sed -r 's/[-+:]//g;s/ /_/g'
+}
 
 # Create writable backup directory
-if [[  ! -d ${BACKUP_DIR} ]]; then
-	mkdir -m 755 -p ${BACKUP_DIR}
+if [[  ! -d "${BACKUP_DIR}" && "${#MYSQL_DATABASES[@]}" -gt 0 ]]; then
+	mkdir -m 755 -p "${BACKUP_DIR}"
 fi
 
 # Change current working directory
-cd ${BACKUP_DIR}
+if [[ -d "${BACKUP_DIR}" ]]; then
+	cd "${BACKUP_DIR}"
+fi
 
-# Create backup
-mysqldump --single-transaction --quick --add-drop-table --replace --log-error=${BASE_DIR}/${MYSQL_LOG} --host=${MYSQL_HOST} --port=${MYSQL_PORT} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --result-file=${BACKUP_FILENAME}.sql ${MYSQL_DATABASE}
+# Backup database schema
+for database in "${MYSQL_DATABASES[@]}"; do
+	mysqldump \
+		--log-error="${BASE_DIR}/$(get_filename 'schema' '%F').log" \
+		--host="${MYSQL_HOST}" \
+		--user="${MYSQL_USER}" \
+		--password="${MYSQL_PASSWORD}" \
+		--result-file="$(get_filename ${database} '' 'schema').sql" \
+		--databases "${database}" \
+		--no-data
+done
 
-if [[ -f ${BACKUP_FILENAME}.sql ]]; then
-	# Create SHA512 file
-	echo "; ${BACKUP_TIMESTAMP}" > ${BACKUP_FILENAME}.sql.sha512
-	sha512sum ${BACKUP_FILENAME}.sql >> ${BACKUP_FILENAME}.sql.sha512
+# Backup database data
+for database in "${MYSQL_DATABASES[@]}"; do
+	mysqldump \
+		--log-error="${BASE_DIR}/$(get_filename 'data' '%F').log" \
+		--host="${MYSQL_HOST}" \
+		--user="${MYSQL_USER}" \
+		--password="${MYSQL_PASSWORD}" \
+		--result-file="$(get_filename ${database} '' 'data').sql" \
+		--databases "${database}" \
+		--single-transaction \
+		--quick \
+		--add-drop-table \
+		--replace \
+		--no-create-db \
+		--no-create-info
+done
 
-	# Compress backup file (XZ)
-	tar cJf ${BACKUP_FILENAME}.tar.xz ${BACKUP_FILENAME}.sql*
-	
-	# Delete SQL and SHA512 file
-	rm -f ${BACKUP_FILENAME}.sql*
+# Compress backup
+if [[ $(find . -maxdepth 1 -type f -name '*.sql' | wc -l) -gt 0 ]]; then
+	name="$(get_filename 'backup')"
+
+	# SHA512
+	sha512sum *.sql > "$(get_filename 'sha512sums').txt"
+
+	# XZ
+	tar cJf "${name}.tar.xz" *.sql *.txt
+
+	# Delete unneeded files
+	if [[ -f "${name}.tar.xz" ]]; then
+		rm -f *.sql *.txt
+	fi
 fi

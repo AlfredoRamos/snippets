66b03f5d50cd2887f2b0f3937ff2e0c59768f3aa
Alfredo Ramos <alfredo.ramos@yandex.com>
2016-11-12 02:52:03 -0600

    [javascript] Add auto-embed script

---
+++ javascript/auto-embed-videos/README.md
@@ -0,0 +1,40 @@
+### Info
+
+A script to auto-embed videos using the *lazy loading* technique
+
+A live demo is located at [`auto-embed-video`](https://alfredoramos.github.io/auto-embed-video/)
+
+### Dependencies
+
+- jQuery [[info](https://jquery.com/download/)]
+- sprintf [[info](https://github.com/alexei/sprintf.js)]
+
+### Usage
+
+Add `auto-embed-services.js` and `auto-embed.js` scripts before `</body>` tag and after `jquery` and `sprintf` lib
+
+**test.html**
+```html
+...
+<head>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.slim.min.js"></script>
+</head>
+...
+<body>
+	<div id="responsive-media" data-url"https://www.youtube.com/watch?v=XXXXXXXXXXXX"></div>
+
+	<!-- Scripts -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/sprintf/1.0.3/sprintf.min.js"></script>
+	<script src="auto-embed-services.js"></script>
+	<script src="auto-embed.js"></script>
+	<script>
+		$(function() {
+			$('.responsive-media').generateEmbedHtml({
+				itemClass: 'responsive-media-item',
+				showUrl: true
+			});
+		});
+	</script>
+</body>
+...
+```
---
+++ javascript/auto-embed-videos/auto-embed.js
@@ -0,0 +1,100 @@
+/**
+ * Auto-embed videos
+ * https://github.com/AlfredoRamos/snippets/tree/master/javascript/auto-embed-videos
+ * @author Alfredo Ramos <alfredo.ramos@yandex.com>
+ * @version 0.1.0
+ * @copyright (c) 2016 Alfredo Ramos
+ * @license GNU GPL-3.0+
+ */
+$(function() {
+	// Get embed HTML
+	$.fn.generateEmbedHtml = function($options) {
+		// Debug
+		console.log(this.length);
+		console.log(typeof($services));
+
+		// Exit if there's no videos to process or if the
+		// $services object is not declared
+		if (!this.length || typeof($services) === 'undefined') {
+			return;
+		}
+
+		// Overwrite settings
+		$options = $.extend({
+			itemClass: '',
+			loadClass: 'load',
+			urlClass: 'video-info',
+			showUrl: false
+		}, $options);
+
+		// Iterate videos
+		$.each(this, function() {
+			var $video = {
+				object: $(this),
+				url: $(this).attr('data-url'),
+				embed: ''
+			};
+
+			// Iterate services
+			$.each($services, function($key, $value) {
+				var $matches = $video.url.match($value.regex);
+				var $html = '<iframe%2$s src="%1$s" frameborder="0" scrolling="no" allowfullscreen="true"></iframe>';
+				var $info = '<pre%2$s>%1$s</pre>';
+
+				if ($matches) {
+					// Get embed URL
+					$video.embed = vsprintf($value.embed, $matches);
+
+					// Service-specific fixes
+					switch($key) {
+						case 'twitch':
+							$video.embed = vsprintf($value.embed, [
+								(typeof($matches[2]) !== 'undefined' ? 'video' : 'channel'),
+								(typeof($matches[2]) !== 'undefined' ? sprintf(
+									'v%s',
+									$matches[2]
+								) : $matches[1])
+							]);
+							break;
+						case 'video':
+							$html = '<video%2$s controls="true"><source src="%1$s" /></video>';
+							break;
+					}
+
+					// Add .load class
+					$video.object.addClass(function() {
+						return $(this).hasClass($options.loadClass) ? false : $options.loadClass;
+					});
+
+					// Generate HTML
+					$video.object.one('mouseover', function() {
+						// Remove .load class
+						$(this).removeClass(function() {
+							return $(this).hasClass($options.loadClass) ? $options.loadClass : false;
+						});
+
+						$video.object.html(vsprintf($html, [
+							$video.embed,
+							(!$options.itemClass.length ? '' : sprintf(
+								' class="%s"',
+								$options.itemClass.replace(/[\.#]/g, '')
+							))
+						]));
+					});
+
+					// Show video URL
+					if ($options.showUrl) {
+						$(vsprintf($info, [
+							$video.url,
+							(!$options.urlClass.length ? '' : sprintf(
+								' class="%s"',
+								$options.urlClass.replace(/[\.#]/g, '')
+							))
+						])).insertAfter($video.object);
+					}
+				}
+			});
+		});
+
+	};
+});

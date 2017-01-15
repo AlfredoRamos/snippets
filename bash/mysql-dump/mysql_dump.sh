d5677ea48d50a2fea2f5678a5969cbea1dded605
Alfredo Ramos <alfredo.ramos@yandex.com>
2017-01-15 15:42:17 -0600

    [bash] Update MySQL dump script
    - Change timestamp format
    - Create `BACKUP_DIR` if it doesn not exist, otherwise update its
    `chmod` permissions
    - Do not check if directory exist before changing to `BACKUP_DIR`, it
    must exist
    - Create `mysqldump` logs inside `BACKUP_DIR`

--- bash/mysql-dump/mysql_dump.sh
+++ bash/mysql-dump/mysql_dump.sh
@@ -29,7 +29,7 @@ get_filename() {
 	local name=''
 
 	if [[ -z "${timestamp}" ]]; then
-		timestamp='%F %T %z'
+		timestamp='%F %T%z'
 	fi
 
 	if [[ ! -z "${prefix}" && ! -z "${suffix}" ]]; then
@@ -46,19 +46,21 @@ get_filename() {
 }
 
 # Create writable backup directory
-if [[  ! -d "${BACKUP_DIR}" && "${#MYSQL_DATABASES[@]}" -gt 0 ]]; then
-	mkdir -m 755 -p "${BACKUP_DIR}"
-fi
+if [[ "${#MYSQL_DATABASES[@]}" -gt 0 ]]; then
+	if [[ ! -d "${BACKUP_DIR}" ]]; then
+		mkdir -m 755 -p "${BACKUP_DIR}"
+	else
+		chmod 755 "${BACKUP_DIR}"
+	fi
 
-# Change current working directory
-if [[ -d "${BACKUP_DIR}" ]]; then
+	# Change current working directory
 	cd "${BACKUP_DIR}"
 fi
 
 # Backup database schema
 for database in "${MYSQL_DATABASES[@]}"; do
 	mysqldump \
-		--log-error="${BASE_DIR}/$(get_filename 'schema' '%F').log" \
+		--log-error="$(get_filename 'schema' '%F').log" \
 		--host="${MYSQL_HOST}" \
 		--port=${MYSQL_PORT} \
 		--user="${MYSQL_USER}" \
@@ -71,7 +73,7 @@ done
 # Backup database data
 for database in "${MYSQL_DATABASES[@]}"; do
 	mysqldump \
-		--log-error="${BASE_DIR}/$(get_filename 'data' '%F').log" \
+		--log-error="$(get_filename 'data' '%F').log" \
 		--host="${MYSQL_HOST}" \
 		--port=${MYSQL_PORT} \
 		--user="${MYSQL_USER}" \
